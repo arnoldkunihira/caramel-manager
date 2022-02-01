fd29d5b33196510f5f97c3ff0f4d979197d40a90
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-02-01 23:58:53 +0300

    Configure pg typeORM database credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UserModule } from "./user/user.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: "postgres",
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            entities: [__dirname + "/**/*.entity{.ts,.js}"],
+            synchronize: process.env.DB_SYNCHRONIZE === "true"
+        }),
+        UserModule
+    ],
     controllers: [AppController],
     providers: [AppService]
 })
